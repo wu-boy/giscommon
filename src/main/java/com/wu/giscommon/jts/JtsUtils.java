44de09550131f47da51f49bd66398cbdad5b4343
wusq <wu.business@qq.com>
2017-11-06 15:49:13 +0800

    add MultiLineString transform to Polygon

--- src/main/java/com/wu/giscommon/jts/JtsUtils.java
+++ src/main/java/com/wu/giscommon/jts/JtsUtils.java
@@ -1,12 +1,10 @@
 package com.wu.giscommon.jts;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.*;
 
 /**
  * JTS工具类
- * @version 2017-09-27
+ * @version 2017-11-06
  */
 public class JtsUtils {
 
@@ -21,5 +19,18 @@ public class JtsUtils {
     public static Point createPoint(double lon, double lat){
         Coordinate c = new Coordinate(lon, lat);
         return geometryFactory.createPoint(c);
+    }
+
+    /**
+     * 根据multiLineString创建Polygon
+     * @param multiLineString
+     * @return Polygon
+     */
+    public static Polygon createPolygon(MultiLineString multiLineString){
+        Polygon result = null;
+        if(multiLineString != null){
+            result = geometryFactory.createPolygon(multiLineString.getCoordinates());
+        }
+        return result;
     }
 }
